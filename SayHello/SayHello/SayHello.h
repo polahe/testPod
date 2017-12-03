85bf1110615c5f5a6e6889a2b24598aa43f65a8f
polahe <497216192@qq.com>
2017-12-03 17:00:39 +0800

    换成.a库看看

---
+++ SayHello/SayHello/SayHello.h
@@ -0,0 +1,15 @@
+//
+//  SayHello.h
+//  testPod
+//
+//  Created by 杨家振 on 2017/12/2.
+//  Copyright © 2017年 杨家振. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SayHello : NSObject
+
+- (void)SayHelloTo;
+
+@end
---
+++ SayHello/SayHello/SayHello.mm
@@ -0,0 +1,20 @@
+//
+//  SayHello.m
+//  testPod
+//
+//  Created by 杨家振 on 2017/12/2.
+//  Copyright © 2017年 杨家振. All rights reserved.
+//
+
+#import "SayHello.h"
+#include "tar.h"
+
+@implementation SayHello
+
+-(void)SayHelloTo
+{
+    NSLog(@"我就是要说哈喽");
+    tarFile(0, nil, "/Users/yangjiazhen/Desktop", "/Users/yangjiazhen/Desktop/1.tar");
+}
+
+@end
